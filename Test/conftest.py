e4137a6b2e6a03449a304b704293736fefc9c752
gonzamol17 <gmolina083@alumnos.iua.edu.ar>
2025-02-13 16:12:14 +0100

    actualizacion para generacion de reportes

--- Test/conftest.py
+++ Test/conftest.py
@@ -10,7 +10,7 @@ driver = None
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
 
-@pytest.fixture()
+@pytest.fixture(scope="class")
 def test_setup(request):
     global driver
     from selenium import webdriver
@@ -34,7 +34,8 @@ def test_setup(request):
     driver.quit()
 
 
-@pytest.mark.hookwrapper
+#@pytest.mark.hookwrapper
+@pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item):
     """
         Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
@@ -46,6 +47,7 @@ def pytest_runtest_makereport(item):
     extra = getattr(report, 'extra', [])
 
     if report.when == 'call' or report.when == "setup":
+        extra.append(pytest_html.extras.url("https://www.automationexercise.com/"))
         xfail = hasattr(report, 'wasxfail')
         if (report.skipped and xfail) or (report.failed and not xfail):
             file_name = report.nodeid.replace("::", "_") + ".png"
@@ -54,9 +56,9 @@ def pytest_runtest_makereport(item):
                 html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
                        'onclick="window.open(this.src)" align="right"/></div>' % file_name
                 extra.append(pytest_html.extras.html(html))
-        report.extra = extra
+        report.extras = extra
 
 
 def _capture_screenshot(name):
     #driver.get_screenshot_as_file(name)
-    driver.get_screenshot_as_file("C:\\Users\\User\\PycharmProjects\\NewAutomationPractice1\\Test\\ScreenShots\\"+name)
+    driver.get_screenshot_as_file("..\\Test\\ScreenShots\\"+name)
